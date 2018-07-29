c4ccf246075799e79244646ad51bcdfb89d093db
koalakoker <koalakoker@gmail.com>
2018-07-29 08:21:03 +0200

    Up and Donw button working

--- src/app/web-pass-list/web-pass-list.component.ts
+++ src/app/web-pass-list/web-pass-list.component.ts
@@ -42,7 +42,34 @@ export class WebPassListComponent implements OnInit {
     this.list.splice(i+1, 0, new WebPass());
   }
 
+  swap(list: WebPass[], i: number, j: number): WebPass[] {
+    const tmp:WebPass = list[j];
+    list[j] = list[i];
+    list[i] = tmp;
+    return list;
+  }
+
+  onButtonUp(webPass: WebPass) {
+    const i: number = this.list.indexOf(webPass);
+    this.swap(this.list,i,i-1);
+  }
+
+  onButtonDown(webPass: WebPass) {
+    const i: number = this.list.indexOf(webPass);
+    this.swap(this.list, i, i + 1);
+  }
+
   isSelected(webPass: WebPass): boolean {
     return (webPass === this.selectedWebPass);
+  }
+
+  isNotFirst(webPass: WebPass): boolean {
+    const i: number = this.list.indexOf(webPass);
+    return (i > 0);
+  }
+
+  isNotLast(webPass: WebPass): boolean {
+    const i: number = this.list.indexOf(webPass);
+    return (i < this.list.length - 1);
   }
 }
