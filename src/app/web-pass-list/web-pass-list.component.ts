45037fabfd9614e7f58155db73a891755ef989a5
koalakoker <koalakoker@gmail.com>
2018-07-31 07:22:25 +0200

    View improvement

--- src/app/web-pass-list/web-pass-list.component.ts
+++ src/app/web-pass-list/web-pass-list.component.ts
@@ -3,6 +3,13 @@ import { WebPassService } from '../services/web-pass.service'
 import { ListComponent } from '../list/list.component';
 import { WebPass } from '../modules/webpass';
 
+enum elementName {
+  EN_URL = 0,
+  EN_PASS = 1,
+  EN_START = 2,
+  EN_STOP = 3
+}
+
 @Component({
   selector: 'app-web-pass-list',
   templateUrl: './web-pass-list.component.html',
@@ -26,12 +33,42 @@ export class WebPassListComponent extends ListComponent implements OnInit {
     this.webPassService.getData().subscribe(data => {this.list = data});
   }
 
-  // onButtonInsert(listElement: Element) {
-  //   const i: number = this.list.indexOf(listElement);
-  //   this.list.splice(i+1, 0, new Element());
-  // }
-  onNewFunc(listElement: Element)
+  onNewFunc(i: number)
   {
-    this.onInsert(listElement, new WebPass());
+    this.onInsert(i, new WebPass());
   }
+
+  getUrl(name: elementName, index: number)
+  {
+    var str: string;
+    var en: boolean;
+    switch (name) {
+      case elementName.EN_URL:
+        str = this.list[index].url;
+        en = str?true:false;
+        str = str?str:"Url";
+        break;
+      case elementName.EN_PASS:
+        str = this.list[index].pass;
+        en = str ? true : false;
+        str = str ? str : "Password";
+        break;
+      case elementName.EN_START:
+        str = this.list[index].registeredDate;
+        en = str ? true : false;
+        str = str ? str : "Registration Date";
+        break;
+      case elementName.EN_STOP:
+        str = this.list[index].expiriatonDate;
+        en = str ? true : false;
+        str = str ? str : "Expiration date";
+        break;
+    
+      default:
+        break;
+    }
+    return { value: str, enabled: en ? "spanColUrl" : "spanColUrlDisabled"};
+  }
+
+
 }
