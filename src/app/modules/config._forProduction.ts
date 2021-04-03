39d8b67598a38c571c84e63af5188558eb393f9f
koalakoker <koalakoker@gmail.com>
2021-04-03 06:28:59 +0200

    Added build script

---
+++ src/app/modules/config._forProduction.ts
@@ -0,0 +1,9 @@
+// Use a local repo for testing
+export function isConfigForTesting(): boolean {
+  return false;
+}
+
+// Skip the time token cript if true
+export function isLocal(): boolean {
+  return false;
+}
