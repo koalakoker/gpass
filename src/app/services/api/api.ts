a6bd714375e97a3492235c70c34f67971b93d7e8
koalakoker <koalakoker@gmail.com>
2021-07-20 05:25:18 +0200

    Using online backend

--- src/app/services/api/api.ts
+++ src/app/services/api/api.ts
@@ -3,7 +3,7 @@ import { LocalService } from '../local.service';
 
 export class Api {
 
-  apiBaseUrl: string = 'http://localhost:5000/api';
+  apiBaseUrl: string = 'https://glinks.herokuapp.com/api';
 
   constructor() {}
 
